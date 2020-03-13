ca2e4af120518777ba3ef460696e6be20fff6c32
Hasan Demirtaş <utsukushihito@outlook.com>
2020-03-13 13:41:36 +0300

    added1.8-1.10 support

---
+++ nms/1_9_R2/src/main/java/com/infumia/fakeplayer/nms/v1_9_R2/FakeCreated1_9_R2.java
@@ -0,0 +1,23 @@
+package com.infumia.fakeplayer.nms.v1_9_R2;
+
+import com.infumia.fakeplayer.api.FakeCreated;
+import com.infumia.fakeplayer.api.INPC;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
+import org.jetbrains.annotations.NotNull;
+
+public final class FakeCreated1_9_R2 implements FakeCreated {
+
+    @NotNull
+    @Override
+    public INPC create(@NotNull final String name, @NotNull final String tabname, @NotNull final World world) {
+        return new NPC(
+            Bukkit.getServer().getOfflinePlayer(name).getUniqueId(),
+            name,
+            tabname,
+            (CraftWorld) world
+        );
+    }
+
+}
