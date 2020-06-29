36c2604496bced89e8c0bcf9290d3e6975ad22a3
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-29 22:14:22 +0300

    fix

--- fakeplayer/src/main/java/io/github/portlek/fakeplayer/FakePlayerAPI.java
+++ fakeplayer/src/main/java/io/github/portlek/fakeplayer/FakePlayerAPI.java
@@ -14,6 +14,7 @@ import io.github.portlek.smartinventory.manager.BasicSmartInventory;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class FakePlayerAPI {
@@ -22,7 +23,7 @@ public final class FakePlayerAPI {
     public final SmartInventory inventory;
 
     @NotNull
-    public final FakePlayer fakePlayer;
+    public final Plugin plugin;
 
     @NotNull
     public final ConfigFile configFile = new ConfigFile();
@@ -36,9 +37,9 @@ public final class FakePlayerAPI {
     @NotNull
     public final MenuFile menuFile = new MenuFile();
 
-    public FakePlayerAPI(@NotNull final FakePlayer fakePlayer) {
-        this.inventory = new BasicSmartInventory(fakePlayer);
-        this.fakePlayer = fakePlayer;
+    public FakePlayerAPI(@NotNull final Plugin plugin) {
+        this.inventory = new BasicSmartInventory(plugin);
+        this.plugin = plugin;
         CfgSection.addProvidedClass(FileElement.class, new FileElement.Provider());
     }
 
@@ -54,7 +55,7 @@ public final class FakePlayerAPI {
                 PlayerJoinEvent.class,
                 event -> event.getPlayer().hasPermission("fakeplayer.version"),
                 event -> this.checkForUpdate(event.getPlayer())
-            ).register(this.fakePlayer);
+            ).register(this.plugin);
             new ListenerBasic<>(
                 PlayerJoinEvent.class,
                 event ->
@@ -62,9 +63,9 @@ public final class FakePlayerAPI {
                         npc.deSpawn();
                         npc.spawn();
                     })
-            ).register(this.fakePlayer);
+            ).register(this.plugin);
         }
-        this.checkForUpdate(this.fakePlayer.getServer().getConsoleSender());
+        this.checkForUpdate(this.plugin.getServer().getConsoleSender());
     }
 
     public void disablePlugin() {
@@ -72,11 +73,11 @@ public final class FakePlayerAPI {
     }
 
     public void checkForUpdate(@NotNull final CommandSender sender) {
-        Bukkit.getScheduler().runTaskAsynchronously(this.fakePlayer, () -> {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
             if (!this.configFile.check_for_update) {
                 return;
             }
-            final UpdateChecker updater = new UpdateChecker(this.fakePlayer, 73139);
+            final UpdateChecker updater = new UpdateChecker(this.plugin, 73139);
 
             try {
                 if (updater.checkForUpdates()) {
@@ -91,7 +92,7 @@ public final class FakePlayerAPI {
                     );
                 }
             } catch (final Exception exception) {
-                this.fakePlayer.getLogger().warning("Update checker failed, could not connect to the API.");
+                this.plugin.getLogger().warning("Update checker failed, could not connect to the API.");
             }
         });
     }
