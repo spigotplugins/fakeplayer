0a3b11bbe98cf844f07fe38949bfe39b0e459972
Hasan Demirtaş <utsukushihito@outlook.com>
2021-12-09 17:41:22 +0300

    Update FakePlayer.java

--- plugin/src/main/java/io/github/portlek/fakeplayer/FakePlayer.java
+++ plugin/src/main/java/io/github/portlek/fakeplayer/FakePlayer.java
@@ -1,11 +1,22 @@
 package io.github.portlek.fakeplayer;
 
+import io.github.portlek.fakeplayer.api.AiBackend;
+import io.github.portlek.fakeplayer.api.AiPlayerCoordinator;
 import org.bukkit.plugin.java.JavaPlugin;
+import tr.com.infumia.infumialib.platform.paper.versionmatched.VersionMatched;
 
 /**
  * a class that represents main class of FakePlayer plugin.
  */
 public final class FakePlayer extends JavaPlugin {
+
+  /**
+   * the backend.
+   */
+  private final AiBackend backend = new VersionMatched<AiBackend>()
+    .of(this)
+    .create(this)
+    .orElseThrow(() -> new IllegalStateException("Something went wrong when creating the backend nms classes, 0x001!"));
 
   @Override
   public void onDisable() {
@@ -13,5 +24,6 @@ public final class FakePlayer extends JavaPlugin {
 
   @Override
   public void onEnable() {
+    AiPlayerCoordinator.backend(this.backend);
   }
 }
