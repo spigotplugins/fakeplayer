6fac520cda04f33f24a5d5dac88de578fa95069e
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-07 14:08:18 +0300

    Update build.gradle.kts

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -9,10 +9,10 @@ dependencies {
   implementation(project(":nms:1_18_R1"))
   implementation(project(":nms:1_18_R2"))
 
-  compileOnlyApi("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
-
   implementation("tr.com.infumia:reflection:0.1.4-SNAPSHOT")
   implementation("tr.com.infumia:versionmatched:0.1.0-SNAPSHOT")
+
+  compileOnlyApi("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
 }
 
 tasks {
