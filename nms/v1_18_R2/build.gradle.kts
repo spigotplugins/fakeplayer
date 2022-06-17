654380e532e98e71411c5a59e35cfa961c078f80
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-17 22:58:18 +0300

    Update build.gradle.kts

--- nms/v1_18_R2/build.gradle.kts
+++ nms/v1_18_R2/build.gradle.kts
@@ -5,5 +5,11 @@ plugins {
 dependencies {
   paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-  implementation("com.github.steveice10:mcprotocollib:1.18.2-1")
+  implementation("com.github.steveice10:mcprotocollib:1.18.2-1") {
+    exclude("net.kyori")
+    exclude("io.netty")
+    exclude("org.junit")
+    exclude("com.google.code.gson")
+    exclude("com.github.spotbugs")
+  }
 }
