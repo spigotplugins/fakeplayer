73bf3f9cad04b6c03ae2a44c3577eef3898b2197
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-18 08:39:11 +0300

    configurable ip/host.

--- plugin/src/main/java/io/github/portlek/fakeplayer/FakePlayerPlugin.java
+++ plugin/src/main/java/io/github/portlek/fakeplayer/FakePlayerPlugin.java
@@ -2,9 +2,11 @@ package io.github.portlek.fakeplayer;
 
 import io.github.portlek.fakeplayer.api.AiBackend;
 import io.github.portlek.fakeplayer.api.AiPlayerCoordinator;
+import io.github.portlek.fakeplayer.api.FakePlayerConfig;
 import io.github.portlek.fakeplayer.nms.v1_18_R2.Backend1_18_R2;
 import java.util.Objects;
 import lombok.AccessLevel;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import org.bukkit.plugin.java.JavaPlugin;
 import tr.com.infumia.versionmatched.VersionMatched;
@@ -20,6 +22,11 @@ public final class FakePlayerPlugin extends JavaPlugin {
     .orElseThrow(() -> new IllegalStateException("Something went wrong when creating the backend nms class!"));
 
   @Override
+  public void onLoad() {
+    this.reloadFiles();
+  }
+
+  @Override
   public void onDisable() {
   }
 
@@ -27,6 +34,11 @@ public final class FakePlayerPlugin extends JavaPlugin {
   public void onEnable() {
     AiPlayerCoordinator.backend(this.backend);
     Objects.requireNonNull(this.getCommand("fakeplayer"), "fakeplayer")
-      .setExecutor(new FakePlayerCommand());
+      .setExecutor(new FakePlayerCommand(this));
+  }
+
+  @SneakyThrows
+  public void reloadFiles() {
+    FakePlayerConfig.init(this.getDataFolder().toPath());
   }
 }
